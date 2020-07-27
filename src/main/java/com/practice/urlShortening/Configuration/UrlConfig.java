1b5f08474eec781664dffd6c1f6a893867da8137
DESKTOP-AM12U9O\MinYoung <qo613@naver.com>
2020-07-27 23:35:03 +0900

    divide controller

--- src/main/java/com/practice/urlShortening/Configuration/UrlConfig.java
+++ src/main/java/com/practice/urlShortening/Configuration/UrlConfig.java
@@ -7,18 +7,30 @@ import com.linecorp.armeria.server.docs.DocService;
 import com.linecorp.armeria.server.logging.AccessLogWriter;
 import com.linecorp.armeria.server.logging.LoggingService;
 import com.linecorp.armeria.spring.ArmeriaServerConfigurator;
-import com.practice.urlShortening.Controller.UrlController;
+import com.practice.urlShortening.Controller.HomeController;
+import com.practice.urlShortening.Controller.UrlDecodeController;
+import com.practice.urlShortening.Controller.UrlDeleteController;
+import com.practice.urlShortening.Controller.UrlRedirectController;
+import com.practice.urlShortening.Controller.UrlRegisterController;
 
 @Configuration
 public class UrlConfig {
 	
 	@Bean
-	public ArmeriaServerConfigurator armeriaServerConfiguraor(UrlController service) {
+	public ArmeriaServerConfigurator armeriaServerConfiguraor(HomeController homeController,
+															  UrlDecodeController urlDecodeController,
+															  UrlRegisterController urlRegisterController,
+															  UrlRedirectController urlRedirectController,
+															  UrlDeleteController urlDeleteController) {
 		return builder -> {
             builder.serviceUnder("/docs", new DocService());
             builder.decorator(LoggingService.newDecorator());
             builder.accessLogWriter(AccessLogWriter.combined(), false);
-			builder.annotatedService(service);
+            builder.annotatedService(homeController);
+			builder.annotatedService(urlDecodeController);
+			builder.annotatedService(urlRegisterController);
+			builder.annotatedService(urlRedirectController);
+			builder.annotatedService(urlDeleteController);
 		};
 	}
 
